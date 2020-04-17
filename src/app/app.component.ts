271cc5317fb7f73e4bbfb9cc911076f3f834ecff
nsasse <nico.sasse@cofinpro.de>
2020-04-17 17:28:07 +0200

    add buy button custom element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, Injector} from '@angular/core';
 import {createCustomElement} from '@angular/elements';
 import {UserDataComponent} from './components/user-data/user-data.component';
+import {BuyButtonComponent} from './components/buy-button/buy-button.component';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,9 @@ export class AppComponent {
   title = 'ba-contract';
 
   constructor(private injector: Injector) {
-    const el = createCustomElement(UserDataComponent, {injector: this.injector});
-    customElements.define('user-data', el);
+    const contract = createCustomElement(UserDataComponent, {injector: this.injector});
+    const buyButton = createCustomElement(BuyButtonComponent, {injector: this.injector});
+    customElements.define('user-data', contract);
+    customElements.define('buy-button', buyButton);
   }
 }
