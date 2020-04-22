7440c0ea641b2f63c8870cf5ea48a5ac627a0943
nsasse <nico.sasse@cofinpro.de>
2020-04-22 23:38:38 +0200

    update custom elements

--- customelements.js
+++ customelements.js
@@ -5,8 +5,8 @@ const concat = require('concat');
     './dist/ba-contract/runtime-es5.js',
     './dist/ba-contract/polyfills-es5.js',
     './dist/ba-contract/main-es5.js',
-  ]
-  await fs.ensureDir('./src/assets/')
-  await concat(files, './src/assets/user-data.js');
+  ];
+  await fs.ensureDir('./src/assets/');
+  await concat(files, './src/assets/contract.js');
   await fs.copyFile('./dist/ba-contract/styles.css', './src/assets/styles.css')
-})()
+})();
